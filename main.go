fa9628c346455a39b2afc45a924c9ffb978a03c4
Richard Geniesse <richard.geniesse@gmail.com>
2019-01-31 14:19:21 -0800

    Support all devices or one device

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ import (
 )
 
 var (
-	apiToken string
-	stdin    *os.File
-	debug    bool
+	token, device string
+	stdin         *os.File
+	allDevices    bool
 )
 
 func main() {
@@ -33,11 +33,22 @@ func configureRootCommand() *cobra.Command {
 		RunE:  run,
 	}
 
-	cmd.Flags().StringVarP(&apiToken,
-		"api.token",
-		"a",
+	cmd.Flags().StringVarP(&token,
+		"token",
+		"t",
 		os.Getenv("PUSHBULLET_APP_TOKEN"),
 		"Pushbullet API app token, use default from PUSHBULLET_APP_TOKEN env var")
+
+	cmd.Flags().StringVarP(&device,
+		"device",
+		"d",
+		os.Getenv("DEVICE"),
+		"A device registered with Pushbullet")
+
+	cmd.Flags().BoolVar(&allDevices,
+		"alldevices",
+		false,
+		"Bool for sending notifications to all devices")
 
 	return cmd
 }
@@ -48,7 +59,7 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("invalid argument(s) received")
 	}
 
-	if apiToken == "" {
+	if token == "" {
 		_ = cmd.Help()
 		return fmt.Errorf("api token is empty")
 	}
@@ -72,16 +83,37 @@ func run(cmd *cobra.Command, args []string) error {
 		return errors.New(err.Error())
 	}
 
-	if err = notifyPushbullet(event); err != nil {
-		return errors.New(err.Error())
+	if allDevices && device == "" {
+
+		if err = notifyPushbulletAll(event); err != nil {
+			return errors.New(err.Error())
+		}
+
+	}
+
+	if !allDevices && device != "" {
+
+		if err = notifyPushbulletOne(event); err != nil {
+			return errors.New(err.Error())
+		}
+
+	}
+
+	if !allDevices && device == "" {
+		fmt.Printf("%s\n", "Must chose to send notification to all devices or one")
+	}
+
+	if allDevices && device != "" {
+		fmt.Printf("%s\n", "Can not send to all device and one device")
 	}
 
 	return nil
 }
 
-func notifyPushbullet(event *types.Event) error {
-	pb := pushbullet.New(apiToken)
-	devs, err := pb.Devices()
+// Send event notification to all devices
+func notifyPushbulletAll(event *types.Event) error {
+	pushall := pushbullet.New(token)
+	devs, err := pushall.Devices()
 	if err != nil {
 		panic(err)
 	}
@@ -89,19 +121,43 @@ func notifyPushbullet(event *types.Event) error {
 	title := fmt.Sprintf("%s/%s", event.Entity.Name, event.Check.Name)
 	message := event.Check.Output
 
-	err = pb.PushNote(devs[0].Iden, title, message)
+	for i := 0; i < len(devs); i++ {
+
+		err = pushall.PushNote(devs[i].Iden, title, message)
+		// Need to add something better here than a panic.
+		// PB seems to hold onto old device Identities.
+		// For now doing nothing seems ok
+		if err != nil {
+			// panic(err)
+		}
+	}
+
+	return nil
+}
+
+func notifyPushbulletOne(event *types.Event) error {
+	pushone := pushbullet.New(token)
+	dev, err := pushone.Device(device)
 	if err != nil {
 		panic(err)
 	}
 
-	if debug == true {
-		log.Println(err)
+	title := fmt.Sprintf("%s/%s", event.Entity.Name, event.Check.Name)
+	message := event.Check.Output
+
+	err = dev.PushNote(title, message)
+	// Need to add something better here than a panic.
+	// PB seems to hold onto old device Identities.
+	// For now doing nothing seems ok
+	if err != nil {
+		// panic(err)
 	}
 
 	return nil
 }
 
 func validateEvent(event *types.Event) error {
+	// Doesn't work for known sample events.
 	// if event.Timestamp <= 0 {
 	// 	return errors.New("timestamp is missing or must be greater than zero")
 	// }
